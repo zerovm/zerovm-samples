d35300de813694713977c0c1d69c79b5d3eebf4c
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2013-06-12 19:10:31 +0300

    issue #31 migration to nvram for hello, readdir, disort. using manifest templates

--- disort/disort.sh
+++ disort/disort.sh
@@ -5,6 +5,8 @@ source ../run.env
 
 ../ns_start.sh 21
 
+ZVM_VERBOSITY=-v2
+
 #config for 10src, 10dst nodes
 SRC_FIRST=1
 SRC_LAST=10
@@ -13,20 +15,20 @@ DST_LAST=10
 
 COUNTER=$SRC_FIRST
 while [  $COUNTER -le $SRC_LAST ]; do
-    ${SETARCH} ${ZEROVM} -Mmanifest/sortsrc"$COUNTER".manifest \
+    ${SETARCH} ${ZEROVM} -Mmanifest/sortsrc"$COUNTER".manifest ${ZVM_VERBOSITY} \
 	> log/zerovm.sortsrc"$COUNTER".log &
     let COUNTER=COUNTER+1 
 done
 
 COUNTER=$DST_FIRST
 while [  $COUNTER -le $DST_LAST ]; do
-    ${SETARCH} ${ZEROVM} -Mmanifest/sortdst"$COUNTER".manifest \
+    ${SETARCH} ${ZEROVM} -Mmanifest/sortdst"$COUNTER".manifest ${ZVM_VERBOSITY} \
 	> log/zerovm.sortdst"$COUNTER".log &
     let COUNTER=COUNTER+1 
 done
 
 date > /tmp/time
-${SETARCH} ${ZEROVM} -Mmanifest/sortman.manifest
+${SETARCH} ${ZEROVM} -Mmanifest/sortman.manifest ${ZVM_VERBOSITY}
 date >> /tmp/time
 
 cat log/sortman.stderr.log
--- disort/genmanifest.sh
+++ disort/genmanifest.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 SCRIPT=$(readlink -f "$0")
-SCRIPT_PATH=`dirname "$SCRIPT"`/
-echo $SCRIPT_PATH 
+SCRIPT_PATH=`dirname "$SCRIPT"`
 
-#Generate from template
+#Generate from template manifests, nvram files
 SRC_FIRST=1
 SRC_LAST=10
 DST_FIRST=1
@@ -12,32 +11,56 @@ DST_LAST=10
 
 SEQUENTIAL_ID=1
 
-sed s/{NODEID}/$COUNTER/g manifest/sortman.manifest.template | \
-sed s@{ABS_PATH}@$SCRIPT_PATH@ | \
-sed s@{SEQUENTIAL_ID}@$SEQUENTIAL_ID@g | \
-sed s@r_man"$COUNTER"-@/dev/in/@g > manifest/sortman.manifest
+NAME=nodeman \
+NODEID=$COUNTER \
+TIMEOUT=100 \
+ABS_PATH=$SCRIPT_PATH \
+SEQUENTIAL_ID=$SEQUENTIAL_ID \
+CHANNELS_INCLUDE=${SCRIPT_PATH}/manifest/sortman.manifest.include \
+../template.sh ../manifest.template | \
+sed s@r_man"$COUNTER"-@/dev/in/@g > manifest/sortman.manifest 
+cp nvram/sortman.nvram.template nvram/sortman.nvram
 
 COUNTER=$SRC_FIRST
 while [  $COUNTER -le $SRC_LAST ]; do
-    sed s/{NODEID}/$COUNTER/g manifest/generator.manifest.template | \
-    sed s@{ABS_PATH}@$SCRIPT_PATH@ | \
-    sed /src"$COUNTER"-src"$COUNTER"/d > manifest/generator"$COUNTER".manifest
+    NAME=generator.uint32_t \
+    TIMEOUT=10 \
+    NODEID=$COUNTER \
+    ABS_PATH=$SCRIPT_PATH \
+    CHANNELS_INCLUDE=${SCRIPT_PATH}/manifest/generator.manifest.include \
+    ../template.sh ../manifest.template > manifest/generator"$COUNTER".manifest
+    cp nvram/generator.nvram.template nvram/generator.nvram
 
     let SEQUENTIAL_ID=SEQUENTIAL_ID+1
-    sed s/{NODEID}/$COUNTER/g manifest/sortsrc.manifest.template | \
-    sed s@{ABS_PATH}@$SCRIPT_PATH@ | \
-    sed s@{SEQUENTIAL_ID}@$SEQUENTIAL_ID@g | \
+#generate manifest
+    NAME=nodesrc \
+    TIMEOUT=100 \
+    NODEID=$COUNTER \
+    ABS_PATH=$SCRIPT_PATH \
+    SEQUENTIAL_ID=$SEQUENTIAL_ID \
+    CHANNELS_INCLUDE=${SCRIPT_PATH}/manifest/sortsrc.manifest.include \
+    ../template.sh ../manifest.template | \
     sed /src"$COUNTER"-src"$COUNTER"/d > manifest/sortsrc"$COUNTER".manifest
+#generate nvram
+    NODEID=$COUNTER \
+    ../template.sh nvram/sortsrc.nvram.template > nvram/"$COUNTER"sortsrc.nvram
     let COUNTER=COUNTER+1 
 done
 
 COUNTER=$DST_FIRST
 while [  $COUNTER -le $DST_LAST ]; do
     let SEQUENTIAL_ID=SEQUENTIAL_ID+1
-    sed s/{NODEID}/$COUNTER/g manifest/sortdst.manifest.template | \
-    sed s@{SEQUENTIAL_ID}@$SEQUENTIAL_ID@g | \
-    sed s@{ABS_PATH}@$SCRIPT_PATH@ > manifest/sortdst"$COUNTER".manifest
+#generate manifest
+    NAME=nodedst \
+    TIMEOUT=100 \
+    NODEID=$COUNTER \
+    ABS_PATH=$SCRIPT_PATH \
+    SEQUENTIAL_ID=$SEQUENTIAL_ID \
+    CHANNELS_INCLUDE=${SCRIPT_PATH}/manifest/sortdst.manifest.include \
+    ../template.sh ../manifest.template > manifest/sortdst"$COUNTER".manifest
+#generate nvram
+    NODEID=$COUNTER \
+    SEQUENTIAL_ID=$SEQUENTIAL_ID \
+    ../template.sh nvram/sortdst.nvram.template > nvram/"$COUNTER"sortdst.nvram
     let COUNTER=COUNTER+1 
 done
-
-sed s@{ABS_PATH}@$SCRIPT_PATH@g manifest/generator.manifest.template > manifest/generator.manifest
